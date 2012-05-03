ea1ce1a68152c3f558a626a7cedc61a546ab4db3
Suneel Chakravorty <suneel0101@gmail.com>
2012-05-02 20:54:58 -0400

    ????SETTINGS?sdfkjhsdfkhsdf??

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.psycopg2_postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
