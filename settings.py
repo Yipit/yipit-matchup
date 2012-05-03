6dbed5a7c4cc4a78f81de7341175a884a3bd8b96
Suneel Chakravorty <suneel0101@gmail.com>
2012-05-02 20:39:32 -0400

    blah

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         'NAME': 'test',                      # Or path to database file if using sqlite3.
         'USER': 'test',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
