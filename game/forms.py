356818fe43f3ac4f5b7f36e26f8c1d06661227d3
Suneel Chakravorty <suneel0101@gmail.com>
2012-05-04 13:56:56 -0400

    dropdown forms should now order alphabetically

--- game/forms.py
+++ game/forms.py
@@ -3,8 +3,8 @@ from game.models import Game, Account
 from django.contrib.auth.models import User
 
 class GameForm(forms.Form):
-	player_1 = forms.ModelChoiceField(label="Player 1", queryset=Account.objects.all(), empty_label="Select Player 1")
-	player_2 = forms.ModelChoiceField(label="Player 2", queryset=Account.objects.all(), empty_label="Select Player 2")
+	player_1 = forms.ModelChoiceField(label="Player 1", queryset=Account.objects.order_by('handle'), empty_label="Select Player 1")
+	player_2 = forms.ModelChoiceField(label="Player 2", queryset=Account.objects.order_by('handle'), empty_label="Select Player 2")
 	score_1 = forms.IntegerField(label="Player 1's Score", max_value=99, min_value=0, widget=forms.TextInput(attrs={'style':'width:50px', 'placeholder':'Score'}))
 	score_2 = forms.IntegerField(label="Player 2's score", max_value=99, min_value=0, widget=forms.TextInput(attrs={'style':'width:50px', 'placeholder':'Score'}))
 
