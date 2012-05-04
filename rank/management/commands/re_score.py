4f3bdd9469a0364a7abf4fd905dcce2c745ee7af
Suneel Chakravorty <suneel0101@gmail.com>
2012-05-03 21:54:10 -0400

    fixed management command

--- rank/management/commands/re_score.py
+++ rank/management/commands/re_score.py
@@ -5,7 +5,7 @@ from rank.engine import RankEngine
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        Game.objects.all().update(rank=99, score=1000)
+        Account.objects.all().update(rank=99, rating=1000)
         for game in Game.objects.all().order_by('date'):        
             engine = RankEngine(game=game)
             engine.update_scores()
