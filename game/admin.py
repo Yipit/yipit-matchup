8f3d57a99f844e938605ef83245ede61cd6fdef8
Suneel Chakravorty <suneel0101@gmail.com>
2012-06-04 19:57:30 -0400

    changed GameAdmin correctly

--- game/admin.py
+++ game/admin.py
@@ -5,7 +5,7 @@ from game.models import Game, Account
 class GameAdmin(admin.ModelAdmin):
     list_display = ('date', 'winner', 'loser', 'winning_score', 'losing_score',)
     list_per_page = 100
-    search_fields = ['winner', 'loser']
+    search_fields = ['winner__handle', 'loser__handle']
 
 admin.site.register(Game, GameAdmin)
 admin.site.register(Account)
