a898f064737e2bb971a88e04c361775c436341b2
suneel0101 <suneel0101@gmail.com>
2012-06-24 14:50:16 -0400

    refixing on_fire after revert deleted it

--- game/models.py
+++ game/models.py
@@ -74,5 +74,5 @@ class Account(models.Model):
 
     @property
     def on_fire(self):
-        last_3_games = Game.objects.order_by('-date')[:3]
-        return len([game for game in last_3_games if game.winner==self]) == 3
+        last_3_games = Game.objects.order_by('-date').filter(Q(winner=self) or Q(loser=self))[:3]
+        return len([game for game in last_3_games if game.winner == self]) == 3
